2e6df740e69042592a4095b009883ab9ff01a9c4
Waquas97 <110435893+Waquas97@users.noreply.github.com>
2024-08-28 12:57:44 -0400

    Add files via upload

---
+++ getdashfiles_apache-server-run.sh
@@ -0,0 +1,9 @@
+wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/CPABE-encrypted-2quality.tar.xz
+mkdir dash
+tar -xf CPABE-encrypted-2quality.tar.xz -C dash
+rm CPABE-encrypted-2quality.tar.xz
+sudo apt update
+sudo apt --assume-yes install apache2
+sudo systemctl start apache2
+sudo sudo mv dash/ /var/www/html
+
