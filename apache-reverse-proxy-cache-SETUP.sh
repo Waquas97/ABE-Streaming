15fb92547ecac06df1b244e9b01a5bb3e03e5f49
Waquas97 <110435893+Waquas97@users.noreply.github.com>
2024-08-29 11:33:54 -0400

    Add files via upload

---
+++ apache-reverse-proxy-cache-SETUP.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get --assume-yes install trafficserver
+wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/records.config
+sudo rm -r /etc/trafficserver/records.config
+sudo mv records.config /etc/trafficserver/ 
+echo 'map http://10.10.1.2 http://10.10.1.1:80' | sudo tee -a /etc/trafficserver/remap.config
+sudo systemctl restart trafficserver
