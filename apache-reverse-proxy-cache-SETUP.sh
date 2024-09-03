e0debb2e7f99106fab924d5b4a6b3427f156d9ec
Waquas97 <110435893+Waquas97@users.noreply.github.com>
2024-09-02 21:30:26 -0400

    Add files via upload

--- apache-reverse-proxy-cache-SETUP.sh
+++ apache-reverse-proxy-cache-SETUP.sh
@@ -6,3 +6,6 @@ sudo rm -r /etc/trafficserver/records.config
 sudo mv records.config /etc/trafficserver/ 
 echo 'map http://10.10.1.2 http://10.10.1.1:80' | sudo tee -a /etc/trafficserver/remap.config
 sudo systemctl restart trafficserver
+
+wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/TC-cache.sh
+bash TC-cache.sh
