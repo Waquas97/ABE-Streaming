dfeefd999258c23e61007070b331275de62154d7
Waquas97 <110435893+Waquas97@users.noreply.github.com>
2024-09-02 21:28:55 -0400

    Add files via upload

---
+++ TC-cache.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#traffic control on server to limit local network interface, this will limit speed to cache, clients,etc everyone.
+sudo tc qdisc del dev $(ifconfig | grep -o 'enp[^:]*') root
+sudo tc qdisc add dev $(ifconfig | grep -o 'enp[^:]*') handle 1: root htb default 11
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: classid 1:1 htb rate 15mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:11 htb rate 5mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:12 htb rate 10mbit
+sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.1 match ip dst 10.10.1.2 flowid 1:12 
