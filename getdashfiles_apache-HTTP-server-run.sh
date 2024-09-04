50265232d8ceea595966ac451169364973aa4451
Waquas97 <110435893+Waquas97@users.noreply.github.com>
2024-09-04 08:57:17 -0400

    Add files via upload

--- getdashfiles_apache-HTTP-server-run.sh
+++ getdashfiles_apache-HTTP-server-run.sh
@@ -1,11 +1,30 @@
-wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/CPABE-encrypted-2quality.tar.xz
-mkdir dash
-tar -xf CPABE-encrypted-2quality.tar.xz -C dash
-rm CPABE-encrypted-2quality.tar.xz
+#!/bin/bash
+
+#get video segments
+cd /var/www/html
+
+# Base URL for the files
+base_url="https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/BigBuckBunny-allI/"
+
+# Outer loop for the streams (0 to 4)
+for stream in {0..4}; do
+    # Inner loop for the chunks (00001 to 00150)
+    for chunk in $(seq -f "%05g" 1 150); do
+        # Construct the file name
+        file_name="enc_allI_chunk-stream${stream}-${chunk}.m4s"
+        
+        # Full URL
+        url="${base_url}${file_name}"
+        
+        # Download the file using wget
+        wget -L "${url}"
+    done
+done
+cd
+
 sudo apt update
 sudo apt --assume-yes install apache2
 sudo systemctl start apache2
-sudo sudo mv dash/ /var/www/html
 
 wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/TC-server.sh
 bash TC-server.sh
