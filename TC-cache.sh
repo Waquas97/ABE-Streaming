0dd7b641659f7b6f873474c9bf7123dc8ef7d722
Waquas97 <110435893+Waquas97@users.noreply.github.com>
2024-09-07 10:19:26 -0400

    Updated TC-cache.sh to give each clinet 5Megabits

--- TC-cache.sh
+++ TC-cache.sh
@@ -2,7 +2,11 @@
 #traffic control on server to limit local network interface, this will limit speed to cache, clients,etc everyone.
 sudo tc qdisc del dev $(ifconfig | grep -o 'enp[^:]*') root
 sudo tc qdisc add dev $(ifconfig | grep -o 'enp[^:]*') handle 1: root htb default 11
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: classid 1:1 htb rate 15mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: classid 1:1 htb rate 25mbit
 sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:11 htb rate 5mbit
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:12 htb rate 10mbit
-sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.1 match ip dst 10.10.1.2 flowid 1:12 
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:12 htb rate 5mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:13 htb rate 5mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:14 htb rate 10mbit
+sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.1 match ip dst 10.10.1.2 flowid 1:14 
+sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.2 match ip dst 10.10.1.3 flowid 1:13 
+sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.2 match ip dst 10.10.1.4 flowid 1:12 
