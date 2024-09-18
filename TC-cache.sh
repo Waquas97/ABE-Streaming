7a9ee0dab2e11a02ab316e54b441c03b1254a27f
Waquas97 <110435893+Waquas97@users.noreply.github.com>
2024-09-18 08:56:49 -0400

    changed bandwidth according to 5x5 clients

--- TC-cache.sh
+++ TC-cache.sh
@@ -2,11 +2,18 @@
 #traffic control on server to limit local network interface, this will limit speed to cache, clients,etc everyone.
 sudo tc qdisc del dev $(ifconfig | grep -o 'enp[^:]*') root
 sudo tc qdisc add dev $(ifconfig | grep -o 'enp[^:]*') handle 1: root htb default 11
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: classid 1:1 htb rate 25mbit
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:11 htb rate 5mbit
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:12 htb rate 5mbit
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:13 htb rate 5mbit
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:14 htb rate 10mbit
-sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.1 match ip dst 10.10.1.2 flowid 1:14 
-sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.2 match ip dst 10.10.1.3 flowid 1:13 
-sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.2 match ip dst 10.10.1.4 flowid 1:12 
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: classid 1:1 htb rate 180mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:11 htb rate 30mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:12 htb rate 30mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:13 htb rate 30mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:14 htb rate 30mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:15 htb rate 30mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:16 htb rate 30mbit
+
+
+sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.1 match ip dst 10.10.1.2 flowid 1:16 
+sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.2 match ip dst 10.10.1.3 flowid 1:15 
+sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.2 match ip dst 10.10.1.4 flowid 1:14 
+sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.2 match ip dst 10.10.1.5 flowid 1:13 
+sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.2 match ip dst 10.10.1.6 flowid 1:12 
+
--- TC-server.sh
+++ TC-server.sh
@@ -2,6 +2,6 @@
 #traffic control on server to limit local netwrok interface, this will limit speed to cache, clients,etc everyone.
 sudo tc qdisc del dev $(ifconfig | grep -o 'enp[^:]*') root
 sudo tc qdisc add dev $(ifconfig | grep -o 'enp[^:]*') handle 1: root htb default 11
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:11 htb rate 10mbit 
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:11 htb rate 30mbit 
 
 
