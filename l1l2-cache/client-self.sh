0028a7cbd86108bffc00bf03c1eab1a2c0ce8bd2
Waquas97 <110435893+Waquas97@users.noreply.github.com>
2024-10-17 12:28:03 -0400

    Add files via upload

---
+++ l1l2-cache/client-self.sh
@@ -0,0 +1,17 @@
+# For me to copy paste on cache node terminal
+wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/l1l2-cache/get-Astream-client-run.sh
+bash get-Astream-client-run.sh
+
+cp -r client/ client1
+cp -r client/ client2
+cp -r client/ client3
+cp -r client/ client4
+cp -r client/ client5
+rm -r client
+
+cp -r https-client/ https-client1
+cp -r https-client/ https-client2
+cp -r https-client/ https-client3
+cp -r https-client/ https-client4
+cp -r https-client/ https-client5
+rm -r https-client
