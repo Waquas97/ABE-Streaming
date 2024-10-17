6b50bb240c44e74ce34b342fe4bb18f3136e336a
Waquas97 <110435893+Waquas97@users.noreply.github.com>
2024-10-17 12:08:00 -0400

    Update server-self.sh

--- l1l2-cache/server-self.sh
+++ l1l2-cache/server-self.sh
@@ -1,5 +1,6 @@
 # For me to copy paste on server node terminal
-wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/getdashfiles_apache-server-run.sh
+wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/l1l2cache-
+getdashfiles_apache-server-run.sh
 bash getdashfiles_apache-server-run.sh 
 
 sudo tail -f /var/log/apache2/access.log | grep '10.10'
