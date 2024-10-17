0438d9708e6f2674e0aa191eeaa5a5238f7926a9
Waquas97 <110435893+Waquas97@users.noreply.github.com>
2024-10-17 11:57:54 -0400

    Add files via upload

--- l1l2-cache/TC-server.sh
+++ l1l2-cache/TC-server.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+#traffic control on server to limit local netwrok interface, this will limit speed to cache, clients,etc everyone.
+sudo tc qdisc del dev $(ifconfig | grep -o 'enp[^:]*') root
+sudo tc qdisc add dev $(ifconfig | grep -o 'enp[^:]*') handle 1: root htb default 11
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:11 htb rate 75mbit 
+
 
