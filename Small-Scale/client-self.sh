5aeb0ddf7cf4fc1c6141a9e2fd5b159912649435
Mohammad Waquas Usmani <110435893+Waquas97@users.noreply.github.com>
2025-02-09 22:04:28 -0500

    Add files via upload

---
+++ Small-Scale/client-self.sh
@@ -0,0 +1,19 @@
+# For me to copy paste on cache node terminal
+wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/Small-Scale/get-Astream-client-run.sh
+bash get-Astream-client-run.sh
+
+cp -r https-client/ https-client1
+cp -r https-client/ https-client2
+cp -r https-client/ https-client3
+cp -r https-client/ https-client4
+cp -r https-client/ https-client5
+rm -r https-client
+
+
+
+cp -r full-client/ full-client1
+cp -r full-client/ full-client2
+cp -r full-client/ full-client3
+cp -r full-client/ full-client4
+cp -r full-client/ full-client5
+rm -r full-client
---
+++ Small-Scale/get-Astream-client-run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Get CPABE toolkit install script and run
+wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/Small-Scale/setup-cpabetoolkit.sh
+bash setup-cpabetoolkit.sh
+
+# Get Astream DASH client
+wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/Small-Scale/https-client.tax.xz
+tar -xf https-client.tax.xz
+rm https-client.tax.xz
+
+# Get Astream DASH client
+wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/Small-Scale/full-client.tax.xz
+tar -xf full-client.tax.xz
+rm full-client.tax.xz
+
