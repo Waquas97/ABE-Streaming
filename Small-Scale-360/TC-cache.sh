af344736edd0da1a3654f627802d6000ec89bc29
Mohammad Waquas Usmani <110435893+Waquas97@users.noreply.github.com>
2025-02-10 22:05:37 -0500

    Add files via upload

---
+++ Small-Scale-360/TC-cache.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#traffic control on server to limit local network interface, this will limit speed to cache, clients,etc everyone.
+sudo tc qdisc del dev $(ifconfig | grep -o 'enp[^:]*') root
+sudo tc qdisc add dev $(ifconfig | grep -o 'enp[^:]*') handle 1: root htb default 11
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: classid 1:1 htb rate 400mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:11 htb rate 60mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:12 htb rate 60mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:13 htb rate 60mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:14 htb rate 60mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:15 htb rate 60mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:16 htb rate 100mbit
+
+
+sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.1 match ip dst 10.10.1.2 flowid 1:16 
+sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.2 match ip dst 10.10.1.3 flowid 1:15 
+sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.2 match ip dst 10.10.1.4 flowid 1:14 
+sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.2 match ip dst 10.10.1.5 flowid 1:13 
+sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.2 match ip dst 10.10.1.6 flowid 1:12 
+
---
+++ Small-Scale-360/apache-reverse-proxy-cache-SETUP.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get --assume-yes install sysstat
+sudo apt-get --assume-yes install -y gcc g++ make libssl-dev tcl libexpat1-dev libpcre3-dev zlib1g-dev libcap-dev libxml2-dev libtool
+sudo apt-get --assume-yes install cmake
+sudo apt-get --assume-yes install pkg-config
+sudo apt-get --assume-yes install libpcre2-8
+sudo apt-get --assume-yes install libpcre2-dev
+git clone https://git-wip-us.apache.org/repos/asf/trafficserver.git
+cd trafficserver/
+git checkout d4dda9b
+cmake -B build -DCMAKE_INSTALL_PREFIX=/opt/ts
+cmake --build build
+cmake --build build -t test
+sudo cmake --install build
+cd /opt/ts
+sudo bin/traffic_server -R 1
+
+cd
+
+wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/Small-Scale-360/records.yaml
+sudo rm /opt/ts/etc/trafficserver/records.yaml
+sudo mv records.yaml /opt/ts/etc/trafficserver/ 
+
+echo 'map https://10.10.1.2:443 https://10.10.1.1:443' | sudo tee -a /opt/ts/etc/trafficserver/remap.config
+echo 'map http://10.10.1.2:80 http://10.10.1.1:80' | sudo tee -a /opt/ts/etc/trafficserver/remap.config
+echo 'dest_ip=* ssl_cert_name=server.crt ssl_key_name=server.key' | sudo tee -a /opt/ts/etc/trafficserver/ssl_multicert.config
+
+sudo /opt/ts/bin/./trafficserver restart
+
+
+wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/Small-Scale-360/TC-cache.sh
+bash TC-cache.sh
