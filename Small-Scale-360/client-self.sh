76a9062c73e780d59483c055ca41ee5b2a3ef4a9
Mohammad Waquas Usmani <110435893+Waquas97@users.noreply.github.com>
2025-02-10 23:00:09 -0500

    Update client-self.sh

--- Small-Scale-360/client-self.sh
+++ Small-Scale-360/client-self.sh
@@ -2,12 +2,12 @@
 wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/Small-Scale-360/get-Astream-client-run.sh
 bash get-Astream-client-run.sh
 
-cp -r https-client/ https-client1
-cp -r https-client/ https-client2
-cp -r https-client/ https-client3
-cp -r https-client/ https-client4
-cp -r https-client/ https-client5
-rm -r https-client
+cp -r 360-https-client/ 360-https-client1
+cp -r 360-https-client/ 360-https-client2
+cp -r 360-https-client/ 360-https-client3
+cp -r 360-https-client/ 360-https-client4
+cp -r 360-https-client/ 360-https-client5
+rm -r 360-https-client
 
 
 
