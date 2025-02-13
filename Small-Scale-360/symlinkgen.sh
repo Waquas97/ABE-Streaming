2f7b6d8c9e772ffb711c210c329e9595b9633537
Mohammad Waquas Usmani <110435893+Waquas97@users.noreply.github.com>
2025-02-13 17:23:26 -0500

    Add files via upload

--- Small-Scale-360/symlinkgen.sh
+++ Small-Scale-360/symlinkgen.sh
@@ -20,18 +20,23 @@ for tile in {1..4}; do
    done
 done
 
-# for copy in {1..4}; do
-#    # Outer loop for the streams (0 to 4)
-#    for stream in {0..4}; do
-#       sudo ln -s "init-stream${stream}.m4s" "init-stream${stream}-copy${copy}.m4s"
-#        # Inner loop for the chunks (00001 to 00150)
-#        for chunk in $(seq -f "%05g" 1 150); do
-#            # Construct the file name
-#            file_name="chunk-stream${stream}-${chunk}.m4s.cpabe"
-#            copy_name="chunk-stream${stream}-${chunk}-copy${copy}.m4s.cpabe"
-        
-#            sudo ln -s $file_name $copy_name
-        
-#        done
-#    done
-# done
+#copy 4 sets
+for scheme in {"allI","allP"};do
+   for tile in {1..4}; do
+      for copy in {1..4}; do
+         # Outer loop for the streams (0 to 4)
+         for stream in {0..4}; do
+            ln -s "init-stream${stream}.m4s" "init-stream${stream}-copy${copy}.m4s"
+            # Inner loop for the chunks (00001 to 00074)
+            for chunk in $(seq -f "%05g" 1 74); do
+               # Construct the file name
+               file_name="tile${tile}-enc_${scheme}_chunk-stream${stream}-${chunk}.m4s"
+               copy_name="tile${tile}-enc_${scheme}_chunk-stream${stream}-${chunk}-copy${copy}.m4s"
+            
+               ln -s $file_name $copy_name
+         
+            done
+         done
+      done
+   done
+done
