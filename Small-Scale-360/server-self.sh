d5bb2c57698beebb1dcd629717fcc51057913054
Mohammad Waquas Usmani <110435893+Waquas97@users.noreply.github.com>
2025-02-12 23:37:19 -0500

    Add files via upload

---
+++ Small-Scale-360/server-self.sh
@@ -0,0 +1,5 @@
+# For me to copy paste on server node terminal
+wget -L https://raw.githubusercontent.com/Waquas97/ABE-Streaming/master/Small-Scale-360/getdashfiles_apache-server-run.sh
+bash getdashfiles_apache-server-run.sh 
+
+sudo tail -f /var/log/apache2/access.log | grep '10.10'
