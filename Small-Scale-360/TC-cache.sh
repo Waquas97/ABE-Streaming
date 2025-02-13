f3a6d74941b4ad5a3dde420454636dd031c56cd5
Mohammad Waquas Usmani <110435893+Waquas97@users.noreply.github.com>
2025-02-12 23:12:23 -0500

    Update TC-cache.sh

--- Small-Scale-360/TC-cache.sh
+++ Small-Scale-360/TC-cache.sh
@@ -2,13 +2,13 @@
 #traffic control on server to limit local network interface, this will limit speed to cache, clients,etc everyone.
 sudo tc qdisc del dev $(ifconfig | grep -o 'enp[^:]*') root
 sudo tc qdisc add dev $(ifconfig | grep -o 'enp[^:]*') handle 1: root htb default 11
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: classid 1:1 htb rate 400mbit
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:11 htb rate 60mbit
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:12 htb rate 60mbit
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:13 htb rate 60mbit
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:14 htb rate 60mbit
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:15 htb rate 60mbit
-sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:16 htb rate 100mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: classid 1:1 htb rate 480mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:11 htb rate 72mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:12 htb rate 72mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:13 htb rate 72mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:14 htb rate 72mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:15 htb rate 72mbit
+sudo tc class add dev $(ifconfig | grep -o 'enp[^:]*') parent 1:1 classid 1:16 htb rate 120mbit
 
 
 sudo tc filter add dev $(ifconfig | grep -o 'enp[^:]*') parent 1: protocol ip prio 1 u32 match ip src 10.10.1.1 match ip dst 10.10.1.2 flowid 1:16 
