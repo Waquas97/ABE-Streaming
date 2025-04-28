56ddd2bf2b4bdf6e73666305062494b50b5f4288
Mohammad Waquas Usmani <110435893+Waquas97@users.noreply.github.com>
2025-04-28 10:45:00 -0400

    Update symlinkgen.sh

--- Small-Scale-360/symlinkgen.sh
+++ Small-Scale-360/symlinkgen.sh
@@ -8,7 +8,7 @@ for tile in {1..9}; do
       for stream in {0..3}; do
          ln -s "init-stream${stream}.m4s" "init-stream${stream}-copy${copy}.m4s"
           # Inner loop for the chunks (00001 to 00074)
-          for chunk in $(seq -f "%05g" 1 74); do
+          for chunk in $(seq -f "%05g" 1 147); do
               # Construct the file name
               file_name="tile${tile}-chunk-stream${stream}-${chunk}.m4s"
               copy_name="tile${tile}-chunk-stream${stream}-${chunk}-copy${copy}.m4s"
@@ -28,7 +28,7 @@ for scheme in {"allI","allP"};do
          for stream in {0..3}; do
             ln -s "init-stream${stream}.m4s" "init-stream${stream}-copy${copy}.m4s"
             # Inner loop for the chunks (00001 to 00074)
-            for chunk in $(seq -f "%05g" 1 74); do
+            for chunk in $(seq -f "%05g" 1 147); do
                # Construct the file name
                file_name="tile${tile}-enc_${scheme}_chunk-stream${stream}-${chunk}.m4s"
                copy_name="tile${tile}-enc_${scheme}_chunk-stream${stream}-${chunk}-copy${copy}.m4s"
